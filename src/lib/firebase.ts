f9024fe8165ff6393e3c1f4277e15b28f84b307b
Yash Prakash Balpande <145757143+yashbalpande@users.noreply.github.com>
2025-07-18 12:42:33 +0530

    feat:edit .1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA4NIEzPyOalpuPpimZ3SNlIBduwlizC3c",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  authDomain: "circuitcode-282b2.firebaseapp.com",
+  projectId: "circuitcode-282b2",
+  storageBucket: "circuitcode-282b2.firebasestorage.app",
+  messagingSenderId: "625858113287",
+  appId: "1:625858113287:web:0ec25dc54d5837c0721fa2",
+  measurementId: "G-H2FXVFQCDD"
 };
 
 const app = initializeApp(firebaseConfig);
